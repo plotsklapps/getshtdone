289d1a9e24243fc51920d59c201c7e76abec1e96
plotsklapps <plotsklapps@gmail.com>
2023-12-27 13:05:41 +0100

    - Debugged a lot of stuff...

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -66,6 +66,15 @@ class HomeScreenState extends ConsumerState<HomeScreen> {
                 Flexible(
                   child: taskList.when(
                     data: (List<Task> taskList) {
+                      // taskList.sort((Task a, Task b) {
+                      //   if (a.isCompleted && !b.isCompleted) {
+                      //     return 1;
+                      //   } else if (!a.isCompleted && b.isCompleted) {
+                      //     return -1;
+                      //   } else {
+                      //     return 0;
+                      //   }
+                      // });
                       return ScrollConfiguration(
                         behavior: const ScrollBehavior().copyWith(
                           dragDevices: <PointerDeviceKind>{
--- lib/widgets/task_card.dart
+++ lib/widgets/task_card.dart
@@ -1,3 +1,4 @@
+import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -6,7 +7,7 @@ import 'package:getsh_tdone/providers/tasklist_provider.dart';
 import 'package:getsh_tdone/providers/theme_provider.dart';
 import 'package:getsh_tdone/theme/theme.dart';
 
-class TaskCard extends ConsumerWidget {
+class TaskCard extends ConsumerStatefulWidget {
   const TaskCard(
     this.task, {
     super.key,
@@ -14,143 +15,182 @@ class TaskCard extends ConsumerWidget {
   final Task task;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<TaskCard> createState() {
+    return TaskCardState();
+  }
+}
+
+class TaskCardState extends ConsumerState<TaskCard> {
+  late ConfettiController confettiController;
+
+  @override
+  void initState() {
+    super.initState();
+    confettiController = ConfettiController(
+      duration: const Duration(seconds: 2),
+    );
+  }
+
+  @override
+  void dispose() {
+    confettiController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final bool isDarkMode = ref.watch(isDarkModeProvider);
-    return Card(
-      margin: const EdgeInsets.only(bottom: 16.0),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12.0),
-        side: BorderSide(
-          color: task.isCompleted
-              ? isDarkMode
-                  ? flexSchemeDark(ref).primary
-                  : flexSchemeLight(ref).primary
-              : isDarkMode
-                  ? flexSchemeDark(ref).outline
-                  : flexSchemeLight(ref).outline,
-          width: 2.0,
-        ),
-      ),
-      child: IntrinsicHeight(
-        child: Row(
-          children: <Widget>[
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(12.0),
-                    bottomLeft: Radius.circular(12.0),
-                  ),
-                  color: task.category == 'personal'
-                      ? isDarkMode
-                          ? flexSchemeDark(ref).primary
-                          : flexSchemeLight(ref).primary
-                      : task.category == 'work'
-                          ? isDarkMode
-                              ? flexSchemeDark(ref).secondary
-                              : flexSchemeLight(ref).secondary
-                          : task.category == 'study'
-                              ? isDarkMode
-                                  ? flexSchemeDark(ref).tertiary
-                                  : flexSchemeLight(ref).tertiary
-                              : flexSchemeLight(ref).primary,
-                ),
-              ),
+    return Stack(
+      children: <Widget>[
+        Card(
+          margin: const EdgeInsets.only(bottom: 16.0),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0),
+            side: BorderSide(
+              color: widget.task.isCompleted
+                  ? isDarkMode
+                      ? flexSchemeDark(ref).primary
+                      : flexSchemeLight(ref).primary
+                  : isDarkMode
+                      ? flexSchemeDark(ref).outline
+                      : flexSchemeLight(ref).outline,
+              width: 2.0,
             ),
-            Expanded(
-              flex: 15,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                child: Column(
-                  children: <Widget>[
-                    Flexible(
-                      child: ListTile(
-                        contentPadding: EdgeInsets.zero,
-                        title: Text(
-                          task.title,
-                          maxLines: 1,
-                          overflow: TextOverflow.fade,
-                          style: TextStyle(
-                            fontSize: 22.0,
-                            color: task.isCompleted
-                                ? isDarkMode
-                                    ? flexSchemeDark(ref).outline
-                                    : flexSchemeLight(ref).outline
-                                : null,
-                            decoration: task.isCompleted
-                                ? TextDecoration.lineThrough
-                                : null,
-                          ),
-                        ),
-                        subtitle: Text(
-                          task.description ?? 'No Description',
-                          maxLines: 3,
-                          overflow: TextOverflow.fade,
-                          style: TextStyle(
-                            fontSize: 14.0,
-                            color: task.isCompleted
-                                ? ref.watch(isDarkModeProvider)
-                                    ? flexSchemeDark(ref).outline
-                                    : flexSchemeLight(ref).outline
-                                : null,
-                            decoration: task.isCompleted
-                                ? TextDecoration.lineThrough
-                                : null,
-                          ),
-                        ),
-                        trailing: task.isCompleted
-                            ? GestureDetector(
-                                onTap: () {
-                                  ref
-                                      .read(taskListProvider.notifier)
-                                      .toggleCompleted(task.id!);
-                                },
-                                child: FaIcon(
-                                  FontAwesomeIcons.circleCheck,
-                                  color: ref.watch(isDarkModeProvider)
-                                      ? flexSchemeDark(ref).primary
-                                      : flexSchemeLight(ref).primary,
-                                  size: 32.0,
-                                ),
-                              )
-                            : GestureDetector(
-                                onTap: () {
-                                  ref
-                                      .read(taskListProvider.notifier)
-                                      .toggleCompleted(task.id!);
-                                },
-                                child: const FaIcon(
-                                  FontAwesomeIcons.circle,
-                                  size: 32.0,
-                                ),
-                              ),
+          ),
+          child: IntrinsicHeight(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(12.0),
+                        bottomLeft: Radius.circular(12.0),
                       ),
+                      color: widget.task.category == 'personal'
+                          ? isDarkMode
+                              ? flexSchemeDark(ref).primary
+                              : flexSchemeLight(ref).primary
+                          : widget.task.category == 'work'
+                              ? isDarkMode
+                                  ? flexSchemeDark(ref).secondary
+                                  : flexSchemeLight(ref).secondary
+                              : widget.task.category == 'study'
+                                  ? isDarkMode
+                                      ? flexSchemeDark(ref).tertiary
+                                      : flexSchemeLight(ref).tertiary
+                                  : flexSchemeLight(ref).primary,
                     ),
-                    const SizedBox(height: 8.0),
-                    const Divider(thickness: 2.0),
-                    const SizedBox(height: 4.0),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  ),
+                ),
+                Expanded(
+                  flex: 15,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                    child: Column(
                       children: <Widget>[
-                        Text(task.category ?? 'No Category'),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.end,
+                        Flexible(
+                          child: ListTile(
+                            contentPadding: EdgeInsets.zero,
+                            title: Text(
+                              widget.task.title,
+                              maxLines: 1,
+                              overflow: TextOverflow.fade,
+                              style: TextStyle(
+                                fontSize: 22.0,
+                                color: widget.task.isCompleted
+                                    ? isDarkMode
+                                        ? flexSchemeDark(ref).outline
+                                        : flexSchemeLight(ref).outline
+                                    : null,
+                                decoration: widget.task.isCompleted
+                                    ? TextDecoration.lineThrough
+                                    : null,
+                              ),
+                            ),
+                            subtitle: Text(
+                              widget.task.description ?? 'No Description',
+                              maxLines: 3,
+                              overflow: TextOverflow.fade,
+                              style: TextStyle(
+                                fontSize: 14.0,
+                                color: widget.task.isCompleted
+                                    ? ref.watch(isDarkModeProvider)
+                                        ? flexSchemeDark(ref).outline
+                                        : flexSchemeLight(ref).outline
+                                    : null,
+                                decoration: widget.task.isCompleted
+                                    ? TextDecoration.lineThrough
+                                    : null,
+                              ),
+                            ),
+                            trailing: widget.task.isCompleted
+                                ? GestureDetector(
+                                    onTap: () {
+                                      ref
+                                          .read(taskListProvider.notifier)
+                                          .toggleCompleted(widget.task.id!);
+                                    },
+                                    child: FaIcon(
+                                      FontAwesomeIcons.circleCheck,
+                                      color: ref.watch(isDarkModeProvider)
+                                          ? flexSchemeDark(ref).primary
+                                          : flexSchemeLight(ref).primary,
+                                      size: 32.0,
+                                    ),
+                                  )
+                                : GestureDetector(
+                                    onTap: () {
+                                      confettiController.play();
+                                      ref
+                                          .read(taskListProvider.notifier)
+                                          .toggleCompleted(widget.task.id!);
+                                    },
+                                    child: const FaIcon(
+                                      FontAwesomeIcons.circle,
+                                      size: 32.0,
+                                    ),
+                                  ),
+                          ),
+                        ),
+                        const SizedBox(height: 8.0),
+                        const Divider(thickness: 2.0),
+                        const SizedBox(height: 4.0),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: <Widget>[
-                            Text('Created: ${task.createdDate!}'),
-                            Text(
-                              'Due: ${task.dueDate!}',
+                            Text(widget.task.category ?? 'No Category'),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: <Widget>[
+                                Text('Created: ${widget.task.createdDate!}'),
+                                Text(
+                                  'Due: ${widget.task.dueDate!}',
+                                ),
+                              ],
                             ),
                           ],
                         ),
                       ],
                     ),
-                  ],
+                  ),
                 ),
-              ),
+              ],
             ),
-          ],
+          ),
         ),
-      ),
+        Align(
+          child: ConfettiWidget(
+            confettiController: confettiController,
+            blastDirectionality: BlastDirectionality.explosive,
+            maxBlastForce: 5.0,
+            minBlastForce: 2.0,
+            emissionFrequency: 0.05,
+            numberOfParticles: 80,
+            gravity: 1.0,
+          ),
+        ),
+      ],
     );
   }
 }
